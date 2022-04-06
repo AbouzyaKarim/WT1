a98b2551e9c9855872703385037520dad131e516
Karim <abouzya_karim@emsi-edu.ma>
2022-04-06 21:18:37 +0000

    Comments

--- src/main/java/ma/emsi/Test.java
+++ src/main/java/ma/emsi/Test.java
@@ -1,6 +1,11 @@
+/*
+comment
+*/
+
 package ma.emsi;
 public class Test{
     public static void main(String[] args){
+        //--------------------------------
         System.out.println("Hello World");
     }
 }
